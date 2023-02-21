ff4ce130d50a9cc288dcaecd3cd5b68e619629b9
Philip Eigen <pceigen@gmail.com>
2023-02-21 00:18:03 -0500

    fixed an issue witht b0 T and its reporting

--- R/PARAMS.R
+++ R/PARAMS.R
@@ -1,6 +1,6 @@
 PARAMS <- function(model) {
   params <- data.frame(
-    Term = c(names(model$coefficients)),
+    Term = c(names(model$coefficients)[1], names(model$coefficients)[2]),
     Estimate = c(model$coefficients[1], model$coefficients[2]),
     SE = c(SEB0(model), SEB1(model)),
     Stat = c(TB0(model), TB1(model)),
--- R/TB0.R
+++ R/TB0.R
@@ -1,4 +1,4 @@
 TB0 <- function(model) {
-  tb0 <- mrout[[1]][[1]]/SEB0(model)
+  tb0 <- model$coefficients[1]/SEB0(model)
   return(tb0)
 }
